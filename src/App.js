ee1340eaa0677a933d8e6236878c46978045f88b
Paulo da Luz <paulo.luz@compasso.com.br>
2019-11-21 08:51:35 -0300

    Estruturando o projeto

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//Pages
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import AcessDenied from './pages/AccessDenied/AcessDenied'
+
+//Components
+import Header from './components/Moleculas/Header/Header';
+import Footer from './components/Moleculas/Footer/Footer';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <HashRouter>
+          <Header />
+          <Switch>
+            <Route path="/" exact={true} component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/acessDenied" component={AcessDenied} />
+            <Route path="*" component={Home} />
+          </Switch>
+          <Footer />
+        </HashRouter>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/pages/AccessDenied/AcessDenied.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class AcessDenied extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
