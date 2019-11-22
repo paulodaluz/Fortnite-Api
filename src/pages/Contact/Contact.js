8e9fea787ec8a046880f13a15da748522e44c5d9
Paulo da Luz <paulo.luz@compasso.com.br>
2019-11-22 09:40:23 -0300

    Página de contato criada

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react';
 import { Button, Form } from "react-bootstrap";
 
+//Import CSS
+import "../../App.css";
+import "./Contact.css";
+
 export default class Contato extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      email: '',
+      assunto: '',
+      mensagem: ''
+    };
+
+  }
+
+  sendFeedback() {
+    var nome = this.state.nome
+    var email = this.state.email
+    var assunto = this.state.assunto
+    var mensagem = this.state.mensagem
+
+    console.log(nome, email, assunto, mensagem)
+
+    alert('Recado enviado, agradecemos seu Feedback!')
+  }
+  
     render() {
         return (
-          <div>
+          <div className='centralizando-pagina-contact'>
             <Form>
               <Form.Group controlId="formBasicPassword">
                 <Form.Label>Nome</Form.Label>
-                <Form.Control type="text-muted" placeholder="Digite seu nome" />
+                <Form.Control type="text-muted" placeholder="Digite seu nome" 
+                  onInput={(e) =>
+                    this.setState({ nome: e.target.value })}/>
               </Form.Group>
 
               <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Digite seu e-mail" />
+                <Form.Control type="email" placeholder="Digite seu e-mail"
+                  onInput={(e) =>
+                    this.setState({ email: e.target.value })} />
                 <Form.Text className="text-muted">
                   Para podermos dar um retorno!
                 </Form.Text>
@@ -24,15 +54,18 @@ export default class Contato extends Component {
                 <Form.Control
                   type="text-muted"
                   placeholder="Digite o assunto"
-                />
+                  onInput={(e) =>
+                    this.setState({ assunto: e.target.value })} />
               </Form.Group>
 
               <Form.Group controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Mensagem</Form.Label>
-                <Form.Control as="textarea" rows="3" placeholder="Digite sua mensagem"/>
+                <Form.Control as="textarea" rows="3" placeholder="Digite sua mensagem"
+                  onInput={(e) =>
+                    this.setState({ mensagem: e.target.value })} />
               </Form.Group>
 
-              <Button variant="primary" type="submit">
+              <Button variant="primary" onClick={() => this.sendFeedback()}  >
                 Enviar
               </Button>
             </Form>
